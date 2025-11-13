52df1bfe060c049a50078fd598f19090d7da39cd
everton-lourens <evertonlourens@gmail.com>
2025-11-13 16:19:33 -0300

    refatorando código jogo da velha

--- meuvetor.java
+++ meuvetor.java
@@ -1,5 +1,26 @@
 // Aluno: Everton Lourenço da Silva (Apenas eu)
 // Matrícula: 200040212
+//////////////////
+// https://github.com/Everton-Lourens/jogo-da-velha
+// OBS::: NÃO FOI utilizado IA em hipótese alguma, pode olhar os "commits" enviados ao github.
+//////////////////
+/*
+Olá, professor!
+Fiz todo o código sem auxílio de IA, pode olhar os commits no meu GitHub:
+https://github.com/Everton-Lourens/jogo-da-velha
+
+Obs:: Na reunião com Osvaldo, ele falou para que eu mostrasse ao senhor que já sei programar, afinal eu já
+dei essa aula antes, porém não foi possível dispensar sua aula esse semestre pelo fato de ter apenas
+80 horas no curso anterior e sua aula ter 90 horas.
+Deixei de ir pra sua aula achando que já poderia dispensar ela, mas me dei
+mal...
+Se o senhor quiser considerar o fato que sei programar para não me reprovar
+por falta, vou ficar feliz, caso contrário, eu entendo não ser possível.
+
+Obs2:: Eu trabalho como motoboy e pago minha própria faculdade, jamais iria
+reprovar propositalmente e jogar dinheiro fora.
+Desde já, agradeço.
+ */
 
 //package vetores;
 
@@ -17,7 +38,23 @@ public class meuvetor {
                 { 7, 8, 9 }
         };
 
-        int i, j, x, y, t, k;
+        int mat2[][] = {
+                { 1, 0, 0, 0, 3 },
+                { 0, 1, 0, 0, 0 },
+                { 0, 0, 1, 0, 0 },
+                { 0, 0, 0, 1, 0 },
+                { 0, 0, 0, 0, 1 },
+        };
+
+        int mat[][] = {
+                { 0, 0, 0, 0, 1 },
+                { 0, 0, 0, 1, 0 },
+                { 0, 0, 1, 0, 0 },
+                { 0, 1, 0, 0, 0 },
+                { 1, 1, 0, 0, 0 },
+        };
+
+        int i, j, x, y, t, k, l, m;
         boolean jogoEncerrado = false;
         // Controle de rodada
         int contarRodada = 1;
@@ -25,13 +62,72 @@ public class meuvetor {
         System.out.println("======== Jogo da Velha ========");
         System.out.println("........... REGRAS ...........");
         System.out.println("- Cada jogador terá sua vez de jogar.");
+        boolean identidade = true;
+        /*
+         * for (l = 0; l < 5; l++) {
+         * for (m = 0; m < 5; m++) {
+         * if (mat[l][l] == 1) {
+         * System.out.println(mat[l][l] + " - " + l + "," + l);
+         * identidade = true;
+         * } else {
+         * identidade = false;
+         * break;
+         * }
+         * if (l != m && identidade == true) {
+         * if (mat[l][m] != 0) {
+         * identidade = false;
+         * break;
+         * } else {
+         * identidade = true;
+         * }
+         * } else {
+         * break;
+         * }
+         * }
+         * }
+         */
+
+        int totalMat = (mat.length - 1);
+        for (l = 0; l < mat.length; l++) {
+            for (m = 0; m < mat.length; m++) {
+                if (mat[l][totalMat] != 1) {
+                    System.out.println("NÃO É !! @@@@@@@@@@@@@@@@@@ 1111");
+                    identidade = false;
+                    break;
+                }
+                System.out.println((mat[l][totalMat] != 1 && mat[l][m] == 1));
+                if (l != m && identidade == true) {
+                    if (mat[l][totalMat] != 1 && mat[l][m] == 1) {
+                        System.out.println("NÃO É !! @@@@@@@@@@@@@@@@@@ 3333::: " + mat[l][m] + " - " + l + "," + m);
+                        identidade = false;
+                        break;
+                    }
+                }
+            }
+            totalMat--;
+        }
+        /*
+         * 1 - 0 4
+         * 1 - 1 3
+         * 1 - 2 2
+         * 1 - 3 1
+         * 1 - 4 0
+         */
+
+        if (identidade == false) {
+            System.out.println("@@@ NÃO é identidade!");
+        } else {
+            System.out.println("@@@ É identidade!");
+        }
+
+        System.out.println("========================");
+
         for (x = 0; x < matPadrao.length; x++) {
             for (y = 0; y < matPadrao[0].length; y++) {
                 System.out.print(matPadrao[x][y] + "  ");
             }
             System.out.println(" ");
         }
-        System.out.println("========================");
 
         for (i = 0; i < matPadrao.length; i++) {
             for (j = 0; j < matPadrao[0].length; j++) {
@@ -136,6 +232,14 @@ public class meuvetor {
                                 System.out.println(" ");
                             }
                         }
+                        ////////// teste //////////
+                        for (l = 0; l < matPadrao.length; l++) {
+                            for (m = 0; m < matPadrao[0].length; m++) {
+
+                            }
+                        }
+                        ///////////////////////////
+
                         ///// Verificando se alguém venceu a partida /////
                         if (matPadrao[0][0] == 0 && matPadrao[0][1] == 0 && matPadrao[0][2] == 0) {
                             jogoEncerrado = true;
